dc216696dcb9ded076b4d3f0b7518186174a9cbf
Bhavya Gupta <guptabhavya2101@gmail.com>
2025-11-16 15:12:44 +0530

    Visual Testing Integration

--- pages/LeftNavigationPage.ts
+++ pages/LeftNavigationPage.ts
@@ -5,11 +5,14 @@ export class LeftNavigationPage {
 
     readonly page: Page;
     readonly pimLink: Locator;
+    readonly orangeHrmLogo: Locator;
+    readonly leftNavigationPanel: Locator;
 
     constructor(page: Page){
         this.page = page;
         this.pimLink = page.getByRole('link', { name: 'PIM' });
-
+        this.orangeHrmLogo = page.getByRole('link', { name: 'client brand banner' });
+        this.leftNavigationPanel = page.locator('div.oxd-sidepanel-body');
     }
 
     /**
